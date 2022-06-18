00f7cdcbaa55fc7048bb8a0e4cd162d135f3500a
Janet Ngei <janet.ngei@student.moringaschool.com>
2022-06-18 23:37:45 +0300

    complete

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+function reverseString(word) {
+    const wordArray = word.split("");
+    const reversedWordArray = wordArray.reverse();
+    const reversedWord = reversedWordArray.join("");
+
+    return reversedWord;
+
 }
 
+function isPalindrome(word) {
+    // Write your algorithm here
+    const reverseWord = reverseString(word);
+    if (word === reverseWord) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+
+
 /* 
+palindrome a word sentence spelt the same way both forward and backward ignoring puncuation cases and spaces.
   Add your pseudocode here
 */
 
@@ -12,14 +31,39 @@ function isPalindrome(word) {
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
+    // add your own custom tests in here
+    console.log("Expecting: true");
+    console.log("=>", isPalindrome("racecar"));
 
-  console.log("");
 
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
+    console.log("");
+
+    console.log("Expecting: false");
+    console.log("=>", isPalindrome("robot"));
+    console.log("");
+
+    console.log("Expecting: true");
+    console.log("=>", isPalindrome("mom"));
+
+    console.log("");
+
+    console.log("Expecting: true");
+    console.log("=>", isPalindrome("abba"));
+
+    console.log("");
+
+    console.log("Expecting: true");
+    console.log("=>", isPalindrome("a"));
+
+    console.log("");
+
+    console.log("Expecting: false");
+    console.log("=>", isPalindrome("hi"));
+
+    console.log("");
+
+    console.log("Expecting: false");
+    console.log("=>", isPalindrome("robot"));
 }
 
 module.exports = isPalindrome;
